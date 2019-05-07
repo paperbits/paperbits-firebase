721ffcf539de865b6e7135b06d5540df93ee80a9
Igor O <ygrik@hotmail.com>
2019-05-07 10:40:26 -0700

    refactored user service

--- src/services/firebaseUserService.ts
+++ src/services/firebaseUserService.ts
@@ -1,17 +1,36 @@
 import { IUserService } from "@paperbits/common/user/IUserService";
+import { IUser } from "@paperbits/common/user/IUser";
 import { FirebaseService } from "./firebaseService";
-
 
 export class FirebaseUserService implements IUserService {
     constructor(private firebaseService: FirebaseService) {
     }
 
-    public async getUserPhotoUrl(): Promise<string> {
+    public async signIn(username: string, password: string): Promise<any> {
+        throw new Error("Method is not supported.");
+    }
+
+    public signOut(): void {
+        throw new Error("Method is not supported.");
+    }
+
+    public isCurrentUserLoggedIn(): boolean {
+        return !!this.firebaseService.authenticatedUser;
+    }
+
+    public async getCurrentUser(): Promise<IUser> {
         await this.firebaseService.getFirebaseRef();
 
         if (!this.firebaseService.authenticatedUser) {
             return null;
         }
-        return this.firebaseService.authenticatedUser.photoURL;
+
+        return { 
+            id: this.firebaseService.authenticatedUser.uid,
+            displayName: this.firebaseService.authenticatedUser.displayName,
+            email: this.firebaseService.authenticatedUser.email,
+            claims: [],
+            photoUrl: this.firebaseService.authenticatedUser.photoURL 
+        };
     }
 }
